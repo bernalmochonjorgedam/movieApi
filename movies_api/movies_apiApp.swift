8b45875bd70107653fdbf55171b6ec76cd25a562
bernalmochonjorgedam <jorgemochonbdam@gmail.com>
2025-02-09 20:07:50 +0000

    Initial Commit

---
+++ movies_api/movies_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  movies_apiApp.swift
+//  movies_api
+//
+//  Created by user255005 on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct movies_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
